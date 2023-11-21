536e3d0f22b7652c2c4f603fbaa795d8256ed8e3
Rustam Aliyev <rustam.aliyev889@gmail.com>
2023-11-21 14:05:21 +0000

    changed user model from username to email

--- payments_prj/payments_prj/settings.py
+++ payments_prj/payments_prj/settings.py
@@ -134,6 +134,8 @@ MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+AUTH_USER_MODEL = "userauths.User"
+
 JAZZMIN_SETTINGS = {
     # "site_title": "Paylio",
     "site_header": "Paylio",
